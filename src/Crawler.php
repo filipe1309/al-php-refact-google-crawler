358eb3fadb8371b7873f1a7beeb0569569af8305
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-26 11:31:27 -0300

    refactor: proxy condition on crawler costructor

--- src/Crawler.php
+++ src/Crawler.php
@@ -26,7 +26,7 @@ class Crawler
     public function __construct(
         GoogleProxyInterface $proxy = null
     ) {
-        $this->proxy = is_null($proxy) ? new NoProxy() : $proxy;
+        $this->proxy = $proxy ?? new NoProxy();
     }
 
     /**
